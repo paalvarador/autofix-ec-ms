0bf8f2b739ad17b00ca1594cc17ce4eed3912163
Pablo Alvarado <paalvarador@gmail.com>
2025-03-27 00:31:08 -0500

    feat: Adding user module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+
+export class CreateUserDto {
+  @ApiProperty({ required: true })
+  firstName: string;
+
+  @ApiProperty({ required: true })
+  lastName: string;
+
+  @ApiProperty({ required: true })
+  email: string;
+
+  @ApiProperty({ required: true })
+  phone: string;
+
+  @ApiProperty({ required: true })
+  password: string;
+
+  @ApiProperty({ required: true, default: 'CUSTOMER' })
+  role: Role = 'CUSTOMER';
+}
