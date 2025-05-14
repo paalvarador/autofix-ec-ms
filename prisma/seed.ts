c58f68bc3e3d7ab2d3d183066e7bf7632b52f07a
Pablo Alvarado <paalvarador@gmail.com>
2025-05-13 23:59:00 -0500

    feat[#0001]: Add migrations for vehicleBrand and vehicleModel tables

---
+++ prisma/seed.ts
@@ -0,0 +1,75 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const brands = [
+  {
+    name: 'Chevrolet',
+    models: ['Aveo', 'Spark', 'Sail', 'D-Max', 'Tracker', 'Captiva'],
+  },
+  {
+    name: 'Kia',
+    models: ['Rio', 'Sportage', 'Picanto', 'Sorento', 'Cerato'],
+  },
+  {
+    name: 'Hyundai',
+    models: ['Accent', 'Tucson', 'Elantra', 'Santa Fe', 'Grand i10', 'Kona'],
+  },
+  {
+    name: 'Toyota',
+    models: ['Hilux', 'Fortuner', 'Corolla', 'Prado', 'Yaris'],
+  },
+  {
+    name: 'Mazda',
+    models: ['BT-50', 'CX-5', 'Mazda 3', 'Mazda 2'],
+  },
+  {
+    name: 'Renault',
+    models: ['Duster', 'Logan', 'Sandero', 'Kwid'],
+  },
+  {
+    name: 'Nissan',
+    models: ['Frontier', 'Versa', 'Sentra', 'X-Trail', 'March'],
+  },
+  {
+    name: 'Ford',
+    models: ['Ranger', 'EcoSport', 'Explorer'],
+  },
+  {
+    name: 'Volkswagen',
+    models: ['Gol', 'Tiguan', 'Polo'],
+  },
+  {
+    name: 'Suzuki',
+    models: ['Vitara', 'Swift', 'Grand Vitara'],
+  },
+];
+
+async function main() {
+  for (const brand of brands) {
+    const createdBrand = await prisma.vehicleBrand.create({
+      data: {
+        name: brand.name,
+      },
+    });
+    for (const model of brand.models) {
+      await prisma.vehicleModel.create({
+        data: {
+          name: model,
+          vehicleBrandId: createdBrand.id,
+        },
+      });
+    }
+  }
+  console.log('Seeding completed');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  .finally(async () => {
+    await prisma.$disconnect().then(() => {});
+  });
