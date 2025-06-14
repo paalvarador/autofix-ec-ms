d6b9c79484612b3165d4bda8e69eefe26268b773
Pablo Alvarado <paalvarado@assanet.com>
2025-06-13 23:04:45 -0500

    feat: Creando el servicio de findAll para obtener todas las cotizaciones

--- src/quotations/quotations.service.ts
+++ src/quotations/quotations.service.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaClientUnknownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateQuotationDto } from './dto/create-quotation.dto';
 
@@ -23,6 +24,40 @@ export class QuotationService {
         {
           StatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
           message: 'An unexpected error ocurred.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAll() {
+    try {
+      const quotations = await this.prisma.quotation.findMany({
+        select: {
+          id: true,
+          customerId: true,
+          estimatedTime: true,
+        },
+        orderBy: {
+          id: 'asc',
+        },
+      });
+
+      return quotations;
+    } catch (error) {
+      if (error instanceof PrismaClientUnknownRequestError) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: `Database error: ${error.message}`,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        {
+          satusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: `An unexpected error ocurred ${error}`,
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
