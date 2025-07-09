cf37e5a48a39b3cf6a614e9df0cfeb7ab0a34b85
Pablo Alvarado <paalvarador@gmail.com>
2025-07-09 00:26:21 -0500

    feat: Implementando los servicios del modulo Labors

--- src/labors/labors.service.ts
+++ src/labors/labors.service.ts
@@ -1,26 +1,209 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateLaborDto } from './dto/create-labor.dto';
 import { UpdateLaborDto } from './dto/update-labor.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientUnknownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class LaborsService {
-  create(createLaborDto: CreateLaborDto) {
-    return 'This action adds a new labor';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createLaborDto: CreateLaborDto) {
+    const { workshopId } = createLaborDto;
+
+    // Verificar que el workshopId existe
+    const workshop = await this.prisma.labor.findUnique({
+      where: { id: workshopId },
+    });
+
+    if (!workshop) {
+      throw new BadRequestException(
+        `The workshop with ID ${workshopId} does not exist`,
+      );
+    }
+
+    try {
+      const labor = this.prisma.labor.create({
+        data: createLaborDto,
+        select: { id: true },
+      });
+
+      return labor;
+    } catch (error) {
+      if (error instanceof PrismaClientUnknownRequestError) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: `Database error: ${error.message}`,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: `An unexpected error occurred ${error}`,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all labors`;
+  async findAll() {
+    try {
+      const labors = await this.prisma.labor.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+        },
+        orderBy: {
+          id: 'asc',
+        },
+      });
+
+      return labors;
+    } catch (error) {
+      if (error instanceof PrismaClientUnknownRequestError) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: `Database error: ${error.message}`,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: `An unexpected error occurred: ${error}`,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} labor`;
+  async findOne(id: string) {
+    try {
+      const labor = await this.prisma.labor.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+        },
+      });
+
+      if (!labor) {
+        throw new NotFoundException(`Labor with ID ${id} not found`);
+      }
+
+      return labor;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      if (error instanceof PrismaClientUnknownRequestError) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: `Database error ${error.message}`,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'An unexpected error occurred, please try again',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateLaborDto: UpdateLaborDto) {
-    return `This action updates a #${id} labor`;
+  async update(id: string, updateLaborDto: UpdateLaborDto) {
+    try {
+      const labor = await this.prisma.labor.findUnique({
+        where: { id },
+      });
+
+      if (!labor) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.NOT_FOUND,
+            message: `Labor with ID ${id} not found`,
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      const updateLabor = await this.prisma.labor.update({
+        where: { id },
+        data: updateLaborDto,
+        select: {
+          id: true,
+        },
+      });
+
+      return updateLabor;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'An unexpected error occurred. Please try again later',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} labor`;
+  async remove(id: string) {
+    try {
+      // Intentamos eliminar la Labor
+      const labor = await this.prisma.labor.delete({
+        where: { id },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!labor) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.NOT_FOUND,
+            message: `Labor with id ${id} not found`,
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return { message: 'Labor deleted successfully', ...labor };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'An unexpected error occurred, please try again',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
