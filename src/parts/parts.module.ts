09fd3e5fc852beb3dec3f523a16f0fd3691dafd9
Pablo Alvarado <paalvarador@gmail.com>
2025-07-08 22:53:49 -0500

    feat: Agregando importacion de PrismaModule en el modulo de Parts

--- src/parts/parts.module.ts
+++ src/parts/parts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PartsService } from './parts.service';
 import { PartsController } from './parts.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PartsController],
   providers: [PartsService],
 })
